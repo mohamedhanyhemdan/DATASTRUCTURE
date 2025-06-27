5d6ef8e5dd088a8df8ea4250eb3d7a382ab5b5b7
mohamed hany <hemdanmohamedhany@gmail.com>
2025-06-27 03:13:51 +0300

    last updates

--- JP.py
+++
@@ -1,6 +0,0 @@
-# Save the plot as a JPEG file
-jpeg_filename = "/mnt/data/fringe_capacitance_plot.jpg"
-plt.savefig(jpeg_filename, format='jpeg', dpi=300, bbox_inches='tight', facecolor='black')
-
-# Provide the file for download
-jpeg_filename
